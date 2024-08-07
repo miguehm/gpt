7af6ab3d0fb8984557e0de9bd757c721b2679533
miguehm <miguel.hernandezmor@alumno.buap.mx>
2024-08-06 22:17:16 -0600

    add: md show properly

--- gpt/main.py
+++ gpt/main.py
@@ -1,32 +1,38 @@
 from openai import OpenAI
 from typer import Typer
+from rich.console import Console
+from rich.markdown import Markdown
 
 app = Typer()
 client = OpenAI()
 
+console = Console()
+
+
 @app.command()
 def main(prompt: str):
     response = client.chat.completions.create(
-      model="gpt-4o",
-      messages=[
-        {
-          "role": "user",
-          "content": [
+        model="gpt-4o",
+        messages=[
             {
-              "type": "text",
-              "text": f"{prompt}"
+              "role": "user",
+              "content": [
+                  {
+                      "type": "text",
+                      "text": f"{prompt}"
+                  }
+              ]
             }
-          ]
-        }
-      ],
-      temperature=1,
-      max_tokens=1024,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0
+        ],
+        temperature=1,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
-    print(response.choices[0].message.content)
-    # print(response)
+    md = Markdown(response.choices[0].message.content)
+    console.print(md)
+
 
 if __name__ == "__main__":
     app()
